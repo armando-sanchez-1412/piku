b3e4df3a9e4e3150e488d37302c5f551a3a3ba9d
ARMANDO DE JESUS SANCHEZ FRANCO <armando.sanchez1@ibm.com>
2021-03-18 19:44:03 -0600

    init

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: null };
+  }
+
+  getUsers() {
+    fetch("/users")
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          users: result
+        });
+      },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  componentWillMount() {
+    this.getUsers();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo"></img>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="ds-loader-header">{this.state.users && this.state.users.map(user => {
+          return (
+            <div className="ds-mar-b-1 ds-pad-t-1 ds-panel ds-panel-rounded">
+              Nombre: {user.name} Edad: {user.age}
+            </div>
+          );
+        })}</div>
+      </div>
+
+    );
+  }
+}
+
+export default App;
